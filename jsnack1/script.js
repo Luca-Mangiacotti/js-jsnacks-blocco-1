f7c13ee8d3c65f637f8d70c6225cae21773b0c36
Luca-Mangiacotti <lucamangiacotti100@gmail.com>
2024-12-03 11:31:27 +0100

    finish 1st exercise

---
+++ jsnack1/script.js
@@ -0,0 +1,10 @@
+const num1 = prompt("inserisci il primo numero")
+const num2 = prompt("inserisci il secondo numero ")
+if ( num1 > num2){
+    alert(" il primo numero è maggiore del secondo")
+} else if (num2 > num1) {
+    alert("il secondo numero è maggiore del primo ")
+
+} else {
+    alert(" i numeri sono uguali")
+}
